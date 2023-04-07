858db44a85a1b67ce7972c2b39c1f3d2f07aa67b
bryan88844 <ongzsbryan@gmail.com>
2023-04-07 16:40:26 +0800

    Init

---
+++ src/main/java/com/networksecurity/demo/NetworkSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.networksecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkSecurityApplication.class, args);
+	}
+
+}
