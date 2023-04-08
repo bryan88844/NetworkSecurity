83092535399d35de8172f8556686c7b8ccb203c3
bryan88844 <ongzsbryan@gmail.com>
2023-04-08 17:48:58 +0800

    saa

--- Node1.java
+++ Node1.java
@@ -1,28 +1,81 @@
-import java.net.*;
-
-import javax.xml.crypto.Data;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Node1 {
-    public static void main(String[] args) throws Exception {
-        // Create a socket to send and receive datagrams
-        DatagramSocket socket = new DatagramSocket();
+    public static void main(String[] args) {
+        try {
+            // Define the destination MAC address as "R1"
+            byte[] dstMAC = {'R', '1'};
+            
+            // Define the source MAC address as "N1"
+            byte[] srcMAC = {'N', '1'};
 
-        // Create a message to send
-        String message = "Hello, Node3!";
-        byte[] buffer = message.getBytes();
-        String sourceIP = "0x1A";
-        String destIP = "0x2B";
+            byte[] dstIP = {'0', 'x', '2', 'A'};
+            byte[] srcIP = {'0', 'x', '1', 'A'};
+
+            byte[] pingProtocol = {'0'};
+            byte[] dataLength = {'1', '7'};
+
+
+            
+            // Define the Ethernet frame data as a byte array
+            byte[] frameData = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, 
+                                (byte) 0x06, (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a, (byte) 0x0b, 
+                                (byte) 0x08, (byte) 0x00, (byte) 0x45, (byte) 0x00, (byte) 0x00, (byte) 0x00
+                                , (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00
+                                , (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+            
+            // Create a new DatagramPacket containing the Ethernet frame data
+            DatagramPacket packet = new DatagramPacket(frameData, frameData.length);
+            
+            // Set the destination address and port to send the packet to the Ethernet emulator
+            packet.setAddress(InetAddress.getByName("127.0.0.1"));
+            packet.setPort(501);
+            
+            String message = "Hello, Node2!";
+            byte[] msg = message.getBytes();
+            
+            
+            // Create a new DatagramSocket to send the packet
+            frameData[0] = dstMAC[0];
+            frameData[1] = dstMAC[1];
+            frameData[2] = srcMAC[0];
+            frameData[3] = srcMAC[1];
+
+            
+
+            frameData[4] = msg[0]; 
+            frameData[5] = msg[1];
+            frameData[6] = msg[2];
+            frameData[7] = msg[3];
+            frameData[8] = msg[4];
+            frameData[9] = msg[5];
+            frameData[10] = msg[6]; 
+            frameData[11] = msg[7];
+            frameData[12] = msg[8];
+            frameData[13] = msg[9];
+            frameData[14] = msg[10];
+            frameData[15] = msg[11];
+            frameData[16] = msg[12];
 
 
 
-        DatagramPacket IPpacket = new DatagramPacket(sourceIP, destIP, buffer, buffer.length);
-
-        String sourcemac = "N1";
-        String destmac = "R1";
-
-        DatagramPacket ethFrame = new DatagramPacket(sourcemac, destmac, IPpacket);
-        socket.send(ethFrame);
-        System.out.println("sending packet");
-        System.out.println(ethFrame);
+            DatagramSocket socket = new DatagramSocket();
+            
+            // Send the packet to the Ethernet emulator
+            System.out.println("Sending packet: ");
+            System.out.println((char)frameData[2] + "" + (char)frameData[3] + "|" + (char)frameData[0] + (char)frameData[1] + "|"
+            + (char)frameData[4] + (char)frameData[5]+ (char)frameData[6]+ (char)frameData[7]+ (char)frameData[8]
+            + (char)frameData[9]+ (char)frameData[10]+ (char)frameData[11]+ (char)frameData[12]+ (char)frameData[13]
+            + (char)frameData[14]+ (char)frameData[15]+ (char)frameData[16]);
+            System.out.println("Packet Sent!");
+            
+            // Close the socket
+            socket.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Router.java
+++ Router.java
@@ -1,6 +1,8 @@
 import java.net.*;
+import java.util.*;
 
 public class Router {
+    
     // Define MAC addresses for each node and router
     private static final byte[] N1_MAC = {'N', '1'};
     private static final byte[] N2_MAC = {'N', '2'};
@@ -11,8 +13,8 @@ public class Router {
     public static void main(String[] args) {
         
         try {
-            // Create a datagram socket for sending and receiving Ethernet frames
-            DatagramSocket ethSocket = new DatagramSocket(500);
+            // Create a DatagramSocket for sending and receiving Ethernet frames
+            DatagramSocket ethSocket = new DatagramSocket(501);
             
             // Create a buffer to hold the Ethernet frame data
             byte[] buffer = new byte[1024];
@@ -22,28 +24,44 @@ public class Router {
             
             // Receive Ethernet frames in a loop
             while (true) {
+                System.out.println("Listening to Ethernet frames...");
                 ethSocket.receive(ethPacket);
+                System.out.println("Receiving incoming packet...");
+   
                 
                 // Get the source and destination MAC addresses from the Ethernet frame
-                byte[] srcMAC = new byte[]{buffer[0], buffer[1]};
-                byte[] dstMAC = new byte[]{buffer[2], buffer[3]};
+                System.out.println("Received ethernet frame: ");
+                byte[] dstMAC = new byte[]{buffer[0], buffer[1]};
+                byte[] srcMAC = new byte[]{buffer[2], buffer[3]};
+                byte[] message = new byte[]{buffer[4], buffer[5], buffer[6],buffer[7],buffer[8],buffer[9],buffer[10],buffer[11],buffer[12],buffer[13],buffer[14],buffer[15],buffer[16],};
+
+
+                System.out.println((char)srcMAC[0] + "" + (char)srcMAC[1] + "|" + (char)dstMAC[0] 
+                + "" + (char)dstMAC[1] + "|" + ethPacket.getLength() + "|" + (char)message[0] + 
+                (char)message[1] + (char)message[2] + (char)message[3]+ (char)message[4]+ (char)message[5]+ (char)message[6]
+                + (char)message[7]+ (char)message[8]+ (char)message[9]+ (char)message[10]+ (char)message[11]+ (char)message[12]
+                );
+
+
                 
                 // Process the Ethernet frame based on its destination MAC address
                 if (Arrays.equals(dstMAC, N1_MAC)) {
+                    System.out.println("Forwarding to N1");
                     // Handle Ethernet frame destined for Node1
-                    System.out.println("Forwarding packet to Node 1!");
                 } else if (Arrays.equals(dstMAC, N2_MAC)) {
+                    System.out.println("Forwarding to N2");
                     // Handle Ethernet frame destined for Node2
-                    System.out.println("Forwarding packet to Node 2!");
                 } else if (Arrays.equals(dstMAC, N3_MAC)) {
+                    System.out.println("Forwarding to N3");
                     // Handle Ethernet frame destined for Node3
-                    System.out.println("Forwarding packet to Node 3!");
                 } else if (Arrays.equals(dstMAC, R1_MAC)) {
-                    System.out.println("Forwarding packet to R1!");
-                } else if (Arrays.equals(dstMAC, R2_MAC)) {
-                    // Handle Ethernet frame destined for Router's second interface
-                    System.out.println("Forwarding packet to R2!");
+                    System.out.println("Forwarding to R1");
 
+                    // Handle Ethernet frame destined for Router's first interface
+                } else if (Arrays.equals(dstMAC, R2_MAC)) {
+                    System.out.println("Forwarding to R2");
+
+                    // Handle Ethernet frame destined for Router's second interface
                 }
             }
             
@@ -51,4 +69,48 @@ public class Router {
             e.printStackTrace();
         }
     }
+    
+    public static void sendEthernetFrame(byte[] data, byte[] srcMAC, byte[] dstMAC) {
+        try {
+            // Create a DatagramPacket to send the Ethernet frame
+            DatagramPacket ethPacket = new DatagramPacket(data, data.length);
+            
+            // Set the source and destination MAC addresses in the Ethernet frame
+            ethPacket.getData()[0] = srcMAC[0];
+            ethPacket.getData()[1] = srcMAC[1];
+            ethPacket.getData()[2] = dstMAC[0];
+            ethPacket.getData()[3] = dstMAC[1];
+            
+            // Set the destination IP address and port to match the destination MAC address
+            if (Arrays.equals(dstMAC, N1_MAC)) {
+                ethPacket.setAddress(InetAddress.getByName("127.0.0.1"));
+                ethPacket.setPort(5001);
+            } else if (Arrays.equals(dstMAC, N2_MAC)) {
+                ethPacket.setAddress(InetAddress.getByName("127.0.0.1"));
+                ethPacket.setPort(5002);
+            } else if (Arrays.equals(dstMAC, N3_MAC)) {
+                ethPacket.setAddress(InetAddress.getByName("127.0.0.1"));
+                ethPacket.setPort(5003);
+            } else if (Arrays.equals(dstMAC, R1_MAC)) {
+                ethPacket.setAddress(InetAddress.getByName("127.0.0.1"));
+                ethPacket.setPort(5004);
+            } else if (Arrays.equals(dstMAC, R2_MAC)) {
+                ethPacket.setAddress(InetAddress.getByName("127.0.0.1"));
+                ethPacket.setPort(5005);
+            }
+            
+            // Create a DatagramSocket to send the Ethernet frame
+            DatagramSocket ethSocket = new DatagramSocket();
+            
+            // Send the Ethernet frame
+            ethSocket.send(ethPacket);
+            
+            // Close the DatagramSocket
+            ethSocket.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+    
