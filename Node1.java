4770b8f1093a4d8677add003ee57bf05bfc756eb
bryan88844 <ongzsbryan@gmail.com>
2023-04-08 20:27:53 +0800

    ???

--- Node1.java
+++ Node1.java
@@ -43,32 +43,50 @@ public class Node1 {
             frameData[2] = srcMAC[0];
             frameData[3] = srcMAC[1];
 
-            
+            frameData[4] = srcIP[0];
+            frameData[5] = srcIP[1];
+            frameData[6] = srcIP[2];
+            frameData[7] = srcIP[3];
 
-            frameData[4] = msg[0]; 
-            frameData[5] = msg[1];
-            frameData[6] = msg[2];
-            frameData[7] = msg[3];
-            frameData[8] = msg[4];
-            frameData[9] = msg[5];
-            frameData[10] = msg[6]; 
-            frameData[11] = msg[7];
-            frameData[12] = msg[8];
-            frameData[13] = msg[9];
-            frameData[14] = msg[10];
-            frameData[15] = msg[11];
-            frameData[16] = msg[12];
+            frameData[8] = dstIP[0];
+            frameData[9] = dstIP[1];
+            frameData[10] = dstIP[2];
+            frameData[11] = dstIP[3];
+
+            frameData[12] = pingProtocol[0];
+            frameData[13] = dataLength[0];
+            frameData[14] = dataLength[1];
+
+
+
+            frameData[15] = msg[0]; 
+            frameData[16] = msg[1];
+            frameData[17] = msg[2];
+            frameData[18] = msg[3];
+            frameData[19] = msg[4];
+            frameData[20] = msg[5];
+            frameData[21] = msg[6]; 
+            frameData[22] = msg[7];
+            frameData[23] = msg[8];
+            frameData[24] = msg[9];
+            frameData[25] = msg[10];
+            frameData[26] = msg[11];
+            frameData[27] = msg[12];
 
 
 
             DatagramSocket socket = new DatagramSocket();
             
             // Send the packet to the Ethernet emulator
+            socket.send(packet);
+
             System.out.println("Sending packet: ");
             System.out.println((char)frameData[2] + "" + (char)frameData[3] + "|" + (char)frameData[0] + (char)frameData[1] + "|"
-            + (char)frameData[4] + (char)frameData[5]+ (char)frameData[6]+ (char)frameData[7]+ (char)frameData[8]
-            + (char)frameData[9]+ (char)frameData[10]+ (char)frameData[11]+ (char)frameData[12]+ (char)frameData[13]
-            + (char)frameData[14]+ (char)frameData[15]+ (char)frameData[16]);
+            + (char)frameData[4] + (char)frameData[5]+ (char)frameData[6]+ (char)frameData[7]+ "|" + (char)frameData[8]
+            + (char)frameData[9]+ (char)frameData[10]+ (char)frameData[11]+ "|" + (char)frameData[12]+ "|" + (char)frameData[13]
+            + (char)frameData[14]+ "|" + (char)frameData[15]+ (char)frameData[16] + (char)frameData[17]+ (char)frameData[18]
+            + (char)frameData[19]+ (char)frameData[20]+ (char)frameData[21]+ (char)frameData[22]+ (char)frameData[23]+ (char)frameData[24]
+            + (char)frameData[25]+ (char)frameData[26]+ (char)frameData[27]);
             System.out.println("Packet Sent!");
             
             // Close the socket
---
+++ Node3.java
@@ -0,0 +1,52 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.*;
+import java.util.*;
+
+public class Node3 {
+    public static void main(String[] args) {
+        
+            try{
+                DatagramSocket ethSocket = new DatagramSocket(503);
+
+                // Create a buffer to hold the Ethernet frame data
+                byte[] buffer = new byte[1024];
+                
+                // Create a DatagramPacket to receive Ethernet frames
+                DatagramPacket ethPacket = new DatagramPacket(buffer, buffer.length);
+    
+                while (true) {
+                    System.out.println("Listening to Ethernet frames...");
+                    ethSocket.receive(ethPacket);
+                    System.out.println("Receiving incoming packet...");
+                    byte[] message = new byte[]{buffer[19],buffer[20],buffer[21],buffer[22],buffer[23],buffer[24],buffer[25],buffer[26],buffer[27],
+                        buffer[28],buffer[29], buffer[30], buffer[31], buffer[32], buffer[33], buffer[34], buffer[35], buffer[36]
+                        , buffer[37], buffer[38], buffer[39], buffer[40], buffer[41], buffer[42]};
+                    // Get the source and destination MAC addresses from the Ethernet frame
+
+                // checking if dest ip is node3 ip, if not drop
+                if ((char)message[7] == (char)'B'){
+                   
+    
+    
+                    System.out.println( (char)message[0] + "" + (char)message[1] + "" + (char)message[2] + "" + (char)message[3]+ "|" 
+                    + (char)message[4] + (char)message[5]+ (char)message[6] + (char)message[7]  + "|" 
+                    + (char)message[8]+ "|" 
+                    + (char)message[9]+ (char)message[10]+ "|" 
+                    + (char)message[11]+ (char)message[12] 
+                    + (char)message[13]+ (char)message[14] + (char)message[15]+ (char)message[16]+ (char)message[17]+ (char)message[18]+ (char)message[19]+ (char)message[20] 
+                    + (char)message[21]+ (char)message[22] + (char)message[23]
+                    );
+                } else {
+                System.out.println("Mismatched Destination IP! Package Dropped! ");
+                }
+
+                }
+    
+            }
+         catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
